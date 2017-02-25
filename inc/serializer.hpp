b2b18d3ef3e5b01b4901f4bbb141eba6bf1a8b5b
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-02-25 12:26:42 +0000

    .

---
+++ inc/serializer.hpp
@@ -0,0 +1,77 @@
+
+# ifndef __serializer__hpp
+# define __serializer__hpp
+# include <boost/cstdint.hpp>
+# include <string.h>
+namespace mdl {
+struct serialize {
+	serialize(char __typ) : typ(__typ) {}
+
+	void operator&(std::size_t __size) {
+		if (this-> typ == 's')
+			this-> obj_size += __size;
+		else this-> obj_size = __size;
+	}
+
+	template<typename __T>
+	void operator<<(__T& __obj) {
+		switch(this-> typ) {
+			case 'r':
+				memset(this-> serial + this-> curr_point, 0x0, this-> obj_size);
+				for (std::size_t o = 0; o != this-> obj_size; o ++) {
+					if (o == 0) this-> serial[this-> curr_point] = __obj & 0xFF;
+					else
+						this-> serial[this-> curr_point] = (__obj >> (o * 8)) & 0xFF;
+
+					this-> curr_point ++;
+				}
+			break;
+			case 'w':
+				for (std::size_t o = 0; o != this-> obj_size; o ++) {
+					__obj |= (__obj & 0xFF) | (this-> serial[this-> curr_point] << (o * 8));
+					this-> curr_point ++;
+				}
+				memset(this-> serial - this-> curr_point, 0x0, this-> obj_size);
+			break;
+		}
+	}
+
+	void reset() {
+		this-> curr_point = 0;
+		this-> obj_size = 0;
+	}
+
+	void operator|(char __typ) {
+		this-> obj_size = 0;
+		this-> curr_point = 0;
+		this-> typ = __typ;
+	}
+
+	void init(std::size_t __bytes) {
+		this-> serial = static_cast<boost::uint8_t *>(malloc(__bytes));
+		memset(this-> serial, 0x0, __bytes);
+	}
+
+	template <typename __T>
+	std::size_t get_size(__T *__struct_ptr) {
+		this-> typ = 's';
+		__struct_ptr-> achieve(*this);
+
+		std::size_t size = this-> obj_size;
+		this-> obj_size = 0;
+		return size;
+	}
+
+	boost::uint8_t* get_serial() {
+		return this-> serial;
+	}
+
+	private:
+	char typ = '\0';
+	std::size_t obj_size = 0;
+	std::size_t curr_point = 0;
+	boost::uint8_t *serial;
+} ;
+}
+
+# endif /*__serializer__hpp*/
